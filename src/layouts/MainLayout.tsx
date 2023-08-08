e3ff6841987fc4ccc2c6f4f7cdbd6aaa9d46fe10
nargis21 <nargisakther1933@gmail.com>
2023-08-08 21:16:00 +0600

    footer done

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import Footer from "./Footer";
 
 
 const MainLayout = () => {
@@ -9,6 +10,7 @@ const MainLayout = () => {
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
             <ToastContainer></ToastContainer>
         </div>
     );
