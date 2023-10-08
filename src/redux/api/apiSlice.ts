42d2d7a432a173d70241806d963ab5c0751da306
nargis21 <nargisakther1933@gmail.com>
2023-10-08 09:42:43 +0600

    backend url updated

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -4,7 +4,7 @@ export const api = createApi({
   reducerPath: "api",
   tagTypes: ["book", "wishlist", "currentList"],
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://book-net-server-orcin.vercel.app/api/v1",
+    baseUrl: "https://book-net-server-orcin.vercel.app/api/v1/",
     prepareHeaders: (headers) => {
       const accessToken = localStorage.getItem("accessToken");
 
